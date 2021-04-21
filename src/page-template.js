cc11bd33337c958a1c4c2d367caae7d07e94e140
Shay Wagner <srwagner916@gmail.com>
2021-04-21 16:20:20 -0500

    site is finished

--- src/page-template.js
+++ src/page-template.js
@@ -12,8 +12,8 @@ const generateAbout = aboutText => {
   `;
 };
 
+// create the projects section
 const generateProjects = projectsArr => {
-  // get array of just featured projects
   return `
     <section class="my-3" id="portfolio">
       <h2 class="text-dark bg-primary p-2 display-inline-block">Work</h2>
@@ -26,18 +26,19 @@ const generateProjects = projectsArr => {
             <h3 class="portfolio-item-title text-light">${name}</h3>
             <h5 class="portfolio-languages">
               Built With:
-              ${languages.join(', ')}
+              ${languages.map(language => language).join(',')}
             </h5>
             <p>${description}</p>
             <a href="${link}" class="btn"><i class="fab fa-github mr-2"></i>View Project on GitHub</a>
-          <div>
+          </div>
         `;
         })
         .join('')}
-        
+
       ${projectsArr
         .filter(({ feature }) => !feature)
         .map(({ name, description, languages, link }) => {
+          console.log(languages);
           return `
           <div class="col-12 col-md-6 mb-2 bg-dark text-light p-3 flex-column">
             <h3 class="portfolio-item-title text-light">${name}</h3>
@@ -51,21 +52,21 @@ const generateProjects = projectsArr => {
         `;
         })
         .join('')}
-        
+    
       </div>
-    <section>
+    </section>
   `;
 };
 
+// export function to generate entire page
 module.exports = templateData => {
-  console.log(templateData);
-  // destructure projecdts and about data from templateData based on their property key names
+  // destructure page data by section
   const { projects, about, ...header } = templateData;
 
   return `
   <!DOCTYPE html>
   <html lang="en">
-
+  
   <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -81,9 +82,7 @@ module.exports = templateData => {
       <div class="container flex-row justify-space-between align-center py-3">
         <h1 class="page-title text-secondary bg-dark py-2 px-3">${header.name}</h1>
         <nav class="flex-row">
-          <a class="ml-2 my-1 px-2 py-1 bg-secondary text-dark" href="https://github.com/${
-            header.github
-          }">GitHub</a>
+          <a class="ml-2 my-1 px-2 py-1 bg-secondary text-dark" href="https://github.com/${header.github}">GitHub</a>
         </nav>
       </div>
     </header>
@@ -92,7 +91,7 @@ module.exports = templateData => {
       ${generateProjects(projects)}
     </main>
     <footer class="container text-center py-3">
-      <h3 class="text-dark">&copy; ${new Date().getFullYear()} by ${header.name}</h3>
+      <h3 class="text-dark">&copy;2020 by ${header.name}</h3>
     </footer>
   </body>
   </html>
